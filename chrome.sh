225c07d2f4bb432b8fa022bb61f919e15a689f81
bls4 <92715010+bls4@users.noreply.github.com>
2023-02-16 15:30:28 +0700

    Update chrome.sh

--- chrome.sh
+++ chrome.sh
@@ -3,8 +3,8 @@ dpkg -i google-chrome-stable_current_amd64.deb;
 rm -rf google-chrome-stable_current_amd64.deb;
 apt --fix-broken install;
 sed -i "s|Exec=/usr/bin/google-chrome-stable %U|Exec=/usr/bin/google-chrome-stable %U --no-sandbox --disable-dev-shm-usage --disable-gpu|g" /usr/share/applications/google-chrome.desktop;
-wget -P ../root/.config https://raw.githubusercontent.com/bls4/chrome/main/google-chrome.zip;
-unzip ../root/.config/google-chrome.zip;
-rm -rf ../root/.config/google-chrome/vtope/*;
-wget -P ../root/.config/google-chrome/vtope https://nextgensoft.ru/bots/youbot/youbot_chrome.zip;
-unzip ../root/.config/google-chrome/vtope/youbot_chrome.zip
+wget -P /root/.config https://raw.githubusercontent.com/bls4/chrome/main/google-chrome.zip;
+unzip /root/.config/google-chrome.zip;
+rm -rf /root/.config/google-chrome/vtope/*;
+wget -P /root/.config/google-chrome/vtope https://nextgensoft.ru/bots/youbot/youbot_chrome.zip;
+unzip /root/.config/google-chrome/vtope/youbot_chrome.zip
