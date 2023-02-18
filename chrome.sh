2bddfdd672dda36efd0d991d79efdb3726fbc51e
bls4 <92715010+bls4@users.noreply.github.com>
2023-02-18 14:26:50 +0700

    Update chrome.sh

--- chrome.sh
+++ chrome.sh
@@ -3,8 +3,9 @@ dpkg -i google-chrome-stable_current_amd64.deb;
 rm -rf google-chrome-stable_current_amd64.deb;
 apt --fix-broken install;
 sed -i "s|Exec=/usr/bin/google-chrome-stable %U|Exec=/usr/bin/google-chrome-stable %U --no-sandbox --disable-dev-shm-usage --disable-gpu|g" /usr/share/applications/google-chrome.desktop;
-wget -P /root/.config https://raw.githubusercontent.com/bls4/chrome/main/google-chrome.zip;
-unzip /root/.config/google-chrome.zip;
-rm -rf /root/.config/google-chrome/vtope/*;
-wget -P /root/.config/google-chrome/vtope https://nextgensoft.ru/bots/youbot/youbot_chrome.zip;
-unzip /root/.config/google-chrome/vtope/youbot_chrome.zip
+cd /root/.config;
+wget https://raw.githubusercontent.com/bls4/chrome/main/google-chrome.zip;
+unzip ./google-chrome.zip;
+rm -rf ./google-chrome/vtope/*;
+wget ./google-chrome/vtope https://nextgensoft.ru/bots/youbot/youbot_chrome.zip;
+unzip ./google-chrome/vtope/youbot_chrome.zip
