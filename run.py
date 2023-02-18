95a7c478bd07c49a809850faeb3549ca234f9240
bls4 <92715010+bls4@users.noreply.github.com>
2023-02-18 17:01:46 +0700

    Create run.py

---
+++ run.py
@@ -0,0 +1,92 @@
+import pyautogui, sys, os
+from time import sleep
+
+
+aaa='/usr/bin/google-chrome-stable %U https://www.youtube.com/create_channel --no-sandbox --disable-dev-shm-usage --disable-gpu &'
+os.system(aaa)
+sleep(4)
+pyautogui.click(x=1415, y=126)
+
+sleep(2)
+pyautogui.click(x=710, y=439)
+
+file = open('mail', 'r')
+usermail = file.read()
+file.close()
+
+file = open('pass', 'r')
+passwd = file.read()
+file.close()
+
+file = open('device', 'r')
+device = file.read()
+file.close()
+
+file = open('token', 'r')
+token = file.read()
+file.close()
+
+# Login gmail
+sleep(3)
+pyautogui.write(usermail) 
+sleep(1)   
+pyautogui.press('enter') 
+sleep(4)
+pyautogui.write(passwd) 
+sleep(1)   
+pyautogui.press('enter')
+
+
+# I understand
+sleep(4)
+pyautogui.click(x=722, y=630)
+
+#click avatar
+sleep(40)
+pyautogui.click(x=1376, y=133)
+
+#click Create channel
+sleep(6)
+pyautogui.click(x=1130, y=249)
+
+# Create channel
+sleep(10)
+pyautogui.click(x=1018, y=752)
+
+
+# click addon
+sleep(20)
+pyautogui.click(x=1290, y=57)
+
+#click token
+sleep(5)
+pyautogui.click(x=984, y=293)
+
+
+#write token
+sleep(1)
+pyautogui.write(token) 
+
+ 
+#click device name
+sleep(1)
+pyautogui.click(x=975, y=380)
+
+#write device name
+sleep(1)
+pyautogui.write(device) 
+
+#click done
+sleep(3)
+pyautogui.click(x=1097, y=453)
+
+#click no proxy
+sleep(35)
+pyautogui.click(x=1091, y=504)
+
+#click make money
+sleep(15)
+pyautogui.click(x=506, y=584)
+
+
+
